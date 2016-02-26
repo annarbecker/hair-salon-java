9b58268bfa68d759c5e6f490a4ee251868272599
Anna Becker <anna.r.becker@gmail.com>
2016-02-26 12:41:01 -0800

    Add integration test for delete stylist.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -17,28 +17,66 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Test
-  // public void rootTest() {
-  //     goTo("http://localhost:4567/");
-  //     assertThat(pageSource()).contains("Hair Salon");
-  // }
-  //
-  // @Test
-  // public void stylistIsCreatedAndDisplayed() {
-  //   goTo("http://localhost:4567/");
-  //   fill("#stylist").with("Jane");
-  //   submit(".btn");
-  //   assertThat(pageSource()).contains("Jane");
-  // }
-  //
-  // @Test
-  // public void clientIsDisplayed() {
-  //   Stylist myStylist = new Stylist("Jane");
-  //   myStylist.save();
-  //   String stylistPath = String.format("http://localhost:4567/stylists/%d", myStylist.getId());
-  //   Client newClient = new Client("Anne", myStylist.getId());
-  //   newClient.save();
-  //   goTo(stylistPath);
-  //   assertThat(pageSource().contains("Anne"));
-  // }
+  @Test
+  public void rootTest() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("Hair Salon");
+  }
+
+  @Test
+  public void stylistIsCreatedAndDisplayed() {
+    goTo("http://localhost:4567/");
+    fill("#stylist").with("Jane");
+    submit("#addStylist");
+    assertThat(pageSource()).contains("Jane");
+  }
+
+  @Test
+  public void clientIsDisplayed() {
+    Stylist myStylist = new Stylist("Jane");
+    myStylist.save();
+    String stylistPath = String.format("http://localhost:4567/stylists/%d", myStylist.getId());
+    Client newClient = new Client("Anne", myStylist.getId());
+    newClient.save();
+    goTo(stylistPath);
+    assertThat(pageSource().contains("Anne"));
+  }
+
+  @Test
+    public void deleteStylist() {
+      Stylist myStylist = new Stylist("Jamie");
+      myStylist.save();
+      goTo("http://localhost:4567/");
+      click(".delete", withText("Delete"));
+      assertThat(!(pageSource()).contains("Jamie"));
+    }
+
+    // @Test
+    // public void removeClientForm_removesClient() {
+    //   Stylist stylist = new Stylist("Grace", "Hopper");
+    //   stylist.save();
+    //   Client client = new Client("Richard", "Roe");
+    //   client.save();
+    //   client.assignStylist(stylist.getId());
+    //   goTo("http://localhost:4567/stylists/" +
+    //     Integer.toString(stylist.getId()));
+    //   click(".btn-link", withText("Remove Richard Roe"));
+    //   assertThat(!(pageSource()).contains("Roe, Richard"));
+    // }
+    //
+    // @Test
+    // public void updateClientForm_ChangesName() {
+    //   Stylist stylist = new Stylist("Grace", "Hopper");
+    //   stylist.save();
+    //   Client client = new Client("Richard", "Roe");
+    //   client.save();
+    //   client.assignStylist(stylist.getId());
+    //   goTo("http://localhost:4567/clients/" +
+    //     Integer.toString(client.getId()));
+    //   fill("#newfirstname").with("Larry");
+    //   fill("#newlastname").with("Loe");
+    //   submit(".btn", withText("Update Name"));
+    //   assertThat(!(pageSource()).contains("Richard Roe"));
+    //   assertThat(pageSource()).contains("Larry Loe");
+    // }
 }
